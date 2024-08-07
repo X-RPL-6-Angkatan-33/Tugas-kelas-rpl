dbc3d24ea0d59d0451f316714f1adf217382fd31
shafirazzahra <shafira_azzahra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:42:40 +0700

    Add files via upload

---
+++ Tugas B.Pasha/ObjekGuru.java
@@ -0,0 +1,57 @@
+public class ObjekGuru {
+    public static void main(String[] args) {
+        //membuat obyek
+        Guru pasha = new Guru();
+        pasha.id = 88;
+        pasha.nama = "pasha";
+        pasha.mapel = "produktif";
+        pasha.alamat = "malang";
+        System.out.println("ini data pasha");
+        System.out.println(pasha.id);
+        System.out.println(pasha.nama);
+        System.out.println(pasha.mapel);
+        System.out.println(pasha.alamat);
+        Guru aul = new Guru();
+        aul.id = 88;
+        aul.nama = "aul";
+        aul.mapel = "produktif";
+        aul.alamat = "malang";
+        System.out.println("ini data aul");
+        System.out.println(aul.id);
+        System.out.println(aul.nama);
+        System.out.println(aul.mapel);
+        System.out.println(aul.alamat);
+        Guru veni = new Guru();
+        veni.id = 90;
+        veni.nama = "veni";
+        veni.mapel = "matematika";
+        veni.alamat = "malang";
+        System.out.println("ini data veni");
+        System.out.println(veni.id);
+        System.out.println(veni.nama);
+        System.out.println(veni.mapel);
+        System.out.println(veni.alamat);
+        Guru diaur = new Guru();
+        diaur.id = 87;
+        diaur.nama = "diaur";
+        diaur.mapel = "olah raga";
+        diaur.alamat = "malang";
+        System.out.println("ini data veni");
+        System.out.println(diaur.id);
+        System.out.println(diaur.nama);
+        System.out.println(diaur.mapel);
+        System.out.println(diaur.alamat); 
+        Guru andre = new Guru();
+        andre.id = 91;
+        andre.nama = "andre";
+        andre.mapel = "bahasa inggris";
+        andre.alamat = "malang";
+        System.out.println("ini data veni");
+        System.out.println(andre.id);
+        System.out.println(andre.nama);
+        System.out.println(andre.mapel);
+        System.out.println(andre.alamat); 
+
+    }
+    
+}
